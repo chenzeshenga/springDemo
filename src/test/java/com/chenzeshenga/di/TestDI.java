f186662d6f09950b8928911e2d02be6c8c085b14
chenzeshenga <chenzeshenga@163.com>
2018-06-29 23:12:38 +0800

    update location

--- src/test/java/com/chenzeshenga/di/TestDI.java
+++ src/test/java/com/chenzeshenga/di/TestDI.java
@@ -6,19 +6,33 @@
 package com.chenzeshenga.di;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 import com.chenzeshenga.di.impl.BookServiceImpl;
 
 public class TestDI {
-	
+
 	@Test
 	public void demo01() {
 		String xmlPath = "beans-di.xml";
+		//加载好xml文件之后就完成bean的创建
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(xmlPath);
 
 		BookServiceImpl bookServiceImpl = (BookServiceImpl) applicationContext.getBean("bookService");
+		bookServiceImpl.addBook();
+	}
+
+	@Test
+	public void demo02() {
+		String xmlPath = "beans-di.xml";
+		//延迟创建bean，在第一次getbean时才创建需要的bean
+		BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource(xmlPath));
+
+		BookServiceImpl bookServiceImpl = (BookServiceImpl) beanFactory.getBean("bookService");
 		bookServiceImpl.addBook();
 	}
 
